8e9f392565204f386f586941de6a0968508c0869
Artucuno1234 <Articuno1234@users.noreply.github.com>
2020-01-20 14:46:08 +1100

    First

---
+++ run.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+
+bot = commands.Bot(command_prefix=config.prefix)
+
+bot.remove_command("help")
+
+@tasks.loop(seconds=18.0)
+async def gameup():
+    svr = bot.get_guild(662541523740786708) # Server
+    bt = svr.get_member(391132891024850945) # Bot
+    channel = bot.get_channel(662541926817333258) # Channel
+    if os.path.isfile('data/status.json'):
+        with open('data/status.json') as json_file:
+            data = json.load(json_file)
+            for p in data['Config']:
+                if bt.status == discord.Status.online:
+                    if p['status'] == "offline":
+                        await channel.send("Bot is online!")
+                        data = {}
+                        data['Config'] = []
+                        data['Config'].append({
+                            'status': "online",
+                            })
+                        with open("data/status.json", 'w+') as outfile:
+                            json.dump(data, outfile)
+                if bt.status == discord.Status.offline:
+                    if p['status'] == "online":
+                        await channel.send("Bot is offline!")
+                        data = {}
+                        data['Config'] = []
+                        data['Config'].append({
+                            'status': "offline",
+                            })
+                        with open("data/status.json", 'w+') as outfile:
+                            json.dump(data, outfile)
+@bot.event
+async def on_ready():
+    print("Started!")
+    
+print("Starting...")
+bot.run(config.token, reconnect=True)
